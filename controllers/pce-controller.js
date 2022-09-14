e1118c631a7e2001023cb621d6de8f1bdc875d78
marcus.martinho <marcus.martinho@tjam.jus.br>
2022-09-14 10:13:16 -0400

    CRUD de PCE OK

--- controllers/pce-controller.js
+++ controllers/pce-controller.js
@@ -1,16 +1,111 @@
 const db = require('../models');
 
+/**
+ * PCEs handling
+ */
 class PCEController {
 
+    /**
+     * List all PCEs with no criteria
+     * 
+     * @param {*} req 
+     * @param {*} res 
+     * @returns PCE[] | string
+     */
     static async PCEs(req, res) {
         try {
             const pces = await db.PCE.findAll();
             return res.status(200).json(pces);
         } catch (error) {
-            return res.status(500).json(error.message); 
+            return res.status(500).json(error); 
         }
     }
 
+    /**
+     * Returns a single PCE object
+     * 
+     * @param {*} req 
+     * @param {*} res 
+     * @returns PCE | string
+     */
+    static async PCE(req, res) {
+        const { id } = req.params;
+        try {
+            const pce = await db.PCE.findOne({ 
+                where: { id: Number(id) } 
+            });
+            return res.status(200).json(pce);
+        } catch (error) {
+            return res.status(500).json(error); 
+        }
+    }    
+
+    /**
+     * Add a single PCE object 
+     * 
+     * @param {*} req 
+     * @param {*} res 
+     * @returns PCE | string
+     */
+    static async criaPCE(req, res) {
+        const PCEdata = req.body;
+        try {
+            const pce = await db.PCE.create(PCEdata);
+            return res.status(200).json(pce);
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    }    
+
+    /**
+     * Update a single PCE object 
+     * 
+     * @param {*} req 
+     * @param {*} res 
+     * @returns PCE | string
+     */
+    static async atualizaPCE(req, res) {
+        const { id } = req.params;
+        const PCEnewData = req.body;
+        try {
+            // update
+            await db.PCE.update(PCEnewData, { 
+                where: { 
+                    id: Number(id) 
+                } 
+            });
+            // search again
+            const updatedPCE = await db.PCE.findOne({ 
+                where: { 
+                    id: Number(id) 
+                } 
+            });            
+            return res.status(200).json(updatedPCE);
+        } catch (error) {
+            return res.status(500).json(error);
+        }        
+    }    
+
+    /**
+     * Delete a single PCE object 
+     * 
+     * @param {*} req 
+     * @param {*} res 
+     * @returns 
+     */
+    static async apagaPCE(req, res) {
+        const { id } = req.params;
+        try {
+            await db.PCE.destroy({
+                where: { 
+                    id: Number(id) 
+                }                 
+            });
+            return res.status(200).json({message:`PCE id ${id} deletado`});
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    }    
 }
 
 module.exports = PCEController;
--- routes/pce-route.js
+++ routes/pce-route.js
@@ -3,11 +3,20 @@ const PCEController = require('../controllers/pce-controller');
 
 const router = Router();
 
+/** 
+ * All PCE routes 
+ */
+
+/** List all PCEs */
 router.get('/pce', PCEController.PCEs);
-//router.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
-//router.post('/pessoas', PessoaController.criaPessoa);
-//router.put('/pessoas/:id', PessoaController.atualizaPessoa);
-//router.delete('/pessoas/:id', PessoaController.apagaPessoa);
+/** Get a single PCE */
+router.get('/pce/:id', PCEController.PCE);
+/** Create a single PCE object */
+router.post('/pce', PCEController.criaPCE);
+/** Update a single PCE object */
+router.put('/pce/:id', PCEController.atualizaPCE);
+/** Delete a single PCE object */
+router.delete('/pce/:id', PCEController.apagaPCE);
 //router.get('/pessoas/:estudanteId/matriculas/:matriculaId', PessoaController.pegaUmaMatricula);
 //router.post('/pessoas/:estudanteId/matriculas', PessoaController.criaMatricula);
 //router.put('/pessoas/:estudanteId/matriculas/:matriculaId', PessoaController.atualizaMatricula);
